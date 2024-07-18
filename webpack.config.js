38ad3fb2513e3000ec775fd15c658437895c6121
EunkyungSon <sek9510@naver.com>
2024-07-19 00:15:54 +0900

    feat: add css & style loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,20 @@ module.exports = {
   mode: "production",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [new BundleAnalyzerPlugin()],
   devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        // loader의 결과를 다음 loader로 전달
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
 
 console.log(process.argv);
