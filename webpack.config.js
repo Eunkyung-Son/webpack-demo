82a1c8fe3153378133ee5ae88dafc102bdef61e8
EunkyungSon <sek9510@naver.com>
2024-07-25 00:43:39 +0900

    using watch mode & webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,23 @@ module.exports = {
     index: "./src/index.js",
     print: "./src/print.js",
   },
-  // devtool: "inline-source-map",
+  devtool: "inline-source-map",
+  /**
+   * webpack-dev-server는 컴파일 후 출력 파일을 작성하지 않습니다.
+   * 대신 번들 파일을 메모리에 보관하고 서버의 루트 경로에 마운트 된 실제 파일인 것처럼 제공합니다.
+   */
+  devServer: {
+    static: "./dist",
+  },
   output: {
     // filename: "bundle.js",
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  /** 단일 html 페이지에 하나 이상의 엔트리 포인트가 있기에 추가 */
+  optimization: {
+    runtimeChunk: "single",
   },
   plugins: [
     // new BundleAnalyzerPlugin(),
