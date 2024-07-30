74032d63a2003171999ffc253d6b72e8e8cc064a
EunkyungSon <sek9510@naver.com>
2024-07-31 00:31:04 +0900

    before code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
   mode: "development",
   entry: {
     index: "./src/index.js",
-    print: "./src/print.js",
+    // print: "./src/print.js",
+    another: "./src/another-module.js",
   },
   devtool: "inline-source-map",
   /**
