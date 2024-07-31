e0747c200c272ebaa4f613ee0e33ee67c9064830
EunkyungSon <sek9510@naver.com>
2024-08-01 01:06:11 +0900

    using entry dependencies dependOn option

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,18 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: {
-    index: "./src/index.js",
+    // index: "./src/index.js",
     // print: "./src/print.js",
-    another: "./src/another-module.js",
+    // another: "./src/another-module.js",
+    index: {
+      import: "./src/index.js",
+      dependOn: "shared",
+    },
+    another: {
+      import: "./src/another-module.js",
+      dependOn: "shared",
+    },
+    shared: "lodash",
   },
   devtool: "inline-source-map",
   /**
@@ -31,7 +40,7 @@ module.exports = {
     runtimeChunk: "single",
   },
   plugins: [
-    // new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin(),
     new HtmlWebpackPlugin({
       // title: "Output Management",
       title: "Development",
